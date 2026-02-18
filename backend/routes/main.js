02197f392a5e9e0ed0e4808b3362cf0472280348
uparfait <parfaituwayo@gmail.com>
2026-02-18 21:24:42 +0200

    Creating base router file too store all routes

---
+++ backend/routes/main.js
@@ -0,0 +1,12 @@
+
+/**
+ * This will be a base files to groupes all routes into the system
+ * And also should never be rewritten again
+ */
+
+const Router = require("express").Router
+const ALL_ROUTES = require('./routes.js')
+
+
+Router.use(ALL_ROUTES)
+module.exports = Router
