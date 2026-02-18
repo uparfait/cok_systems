20ba818e7dfe2e58f2f2f6d75f21600893921e72
uparfait <parfaituwayo@gmail.com>
2026-02-18 21:01:56 +0200

    Verifying database connection

--- backend/db_connection/main.js
+++ backend/db_connection/main.js
@@ -1,13 +1,53 @@
-require("dotenv").config()
+const mongoose = require("mongoose");
+
+// Configuring dotenv module in silent mode.
+require("dotenv").config({quiet: true});
+
+/*
+
+ By default, Node.js often uses Operating System’s DNS settings, which might be pointing to a local router
+  (like 192.168.1.1) or a limited ISP resolver that doesn't support SRV records properly.
+
+  Then we have to tell Node.js to ignore the local/system settings and use Google (8.8.8.8) or Cloudflare (1.1.1.1) instead. 
+  These are high-performance public servers guaranteed to handle the complex SRV and TXT records MongoDB Atlas requires.
+
+  We do this by defollowing lines of codes 
+*/
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 
 
-// creating mongodb connection configurations
+// Configuration constants
+const conne_user = process.env.conne_user || 'cok_systems';
+const conne_password = process.env.conne_password || 'kigalicity';
+const conne_app_name = process.env.conne_app_name || 'cok_systems';
+const dev_mode_conne_string = `mongodb+srv://${conne_user}:${conne_password}@coksystems.rldhlb3.mongodb.net/?appName=coksystems`;
+const conne_string = process.env.conne_string || dev_mode_conne_string;
 
-const conne_user = process.env.conne_user || 'cok_systems'
-const conne_password = process.env.conne_password || 'kigalicity'
-const conne_app_name = process.env.conne_app_name || 'cok_systems'
-const dev_mode_conne_string = `mongodb+srv://${conne_user}:${conne_password}@${conne_user}.rldhlb3.mongodb.net/?appName=${conne_app_name}`
-const conne_string = process.env.conne_string || dev_mode_conne_string
+/**
+ * Connects to MongoDB and returns a response object
+ */
+const connect_db = async () => {
+    try {
+        const conn = await mongoose.connect(conne_string);
 
+        return {
+            status: true,
+            message: "successfully connected to database",
+            connection: conn.connection,
+            host: conn.connection.host,
+            db_name: conn.connection.name,
+        };
+    } catch (error) {
+        return {
+            status: false,
+            message: "database connection failed",
+            error: error,
+            connection: null,
+            conne_string: conne_string
+        };
+    }
+};
 
+module.exports = connect_db;
