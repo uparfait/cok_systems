ed54a740a29a4d7479e71e61a803ca4d89eaedb8
HOMSINdayizeye <nigezamos13@gmail.com>
2026-02-19 17:05:48 +0200

    Remove unwanted routes

--- backend/routes/auth/routes.js
+++ backend/routes/auth/routes.js
@@ -9,30 +9,3 @@ const passwordReset = require("./password-reset/routes.js")
 Router.use('/login', login)
 Router.use('/logout', logout)
 Router.use('/password-reset', passwordReset)
-
-// Keep old routes for backward compatibility
-Router.get('/login', (req, res, next) => {
-    return res.status(200).json({
-        status: true,
-        error: null,
-        message: "GET auth/login"
-    })
-})
-
-Router.post('/login', (req, res, next) => {
-    return res.status(200).json({
-        status: true,
-        error: null,
-        message: "POST auth/login"
-    })
-})
-
-Router.put('/login', (req, res, next) => {
-    return res.status(200).json({
-        status: true,
-        error: null,
-        message: "PUT auth/login"
-    })
-})
-
-module.exports = Router
