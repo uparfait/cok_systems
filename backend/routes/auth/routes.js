b78ee753fbad9ef34c23e1d6049254553789f811
uparfait <parfaituwayo@gmail.com>
2026-02-19 08:57:09 +0200

    Testing auth routes

---
+++ backend/routes/auth/routes.js
@@ -0,0 +1,27 @@
+const Router = require("express").Router()
+
+
+Router.get('/login', (req, res, next) => {
+    return res.status(200).json({
+        status: true,
+        error: null,
+        message: "GET auth/login"
+    })
+})
+
+Router.post('/login', (req, res, next) => {
+    return res.status(200).json({
+        status: true,
+        error: null,
+        message: "POST auth/login"
+    })
+})
+
+Router.put('/login', (req, res, next) => {
+    return res.status(200).json({
+        status: true,
+        error: null,
+        message: "PUT auth/login"
+    })
+})
+
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,11 +6,12 @@ const Router = require('express').Router()
 
 const service_delivary = require("./service_delivary/routes.js")
 const smartparking = require("./smartparking/routes.js")
+const auth = require("./auth/routes.js")
 
 
 Router.use('/servicedelivary', service_delivary)
 Router.use("/smartparking", smartparking)
-
+Router.use("/auth", auth)
 
 
 
