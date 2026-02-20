a6a59ec08baecf389cab165eb1244b7c195f5b23
MUSENGIMANA Fabrice <137602235+MFabrice001@users.noreply.github.com>
2026-02-20 10:45:28 +0200

    emergency car history update

--- backend/models/emergency_car_history.js
+++ backend/models/emergency_car_history.js
@@ -11,6 +11,7 @@ const emergency_car_history_schema = new mongoose.Schema({
             number: String
         },
         telephone_number: String,
+        slot_number: String,
         is_flagged: { type: Boolean, default: false }
     }],
     validity: {
