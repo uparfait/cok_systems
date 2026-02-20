aca76a5959199a3f8d9e3f7700832e352d9920a6
uparfait <parfaituwayo@gmail.com>
2026-02-20 20:04:56 +0200

    Configuring employee crud routes

---
+++ backend/routes/employee_crud/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Below are routes for employee-clud system
+ */
+
+const Router = require('express').Router()
+
+
+/**
+ * import all routes
+ */
+
+const create_employee = require('../../controllers/employee_crud/create_employee.js')
+const list_all_employees = require('../../controllers/employee_crud/get_all_employees.js')
+const get_employee_by_id = require('../../controllers/employee_crud/get_employee_by_id.js')
+const update_employee = require('../../controllers/employee_crud/update_employee.js')
+const delete_employee = require('../../controllers/employee_crud/delete_employee.js')
+const multer = require('multer')
+const upload = multer()
+
+
+Router.use(upload.any())
+Router.get('/', list_all_employees)
+Router.get('/:id', get_employee_by_id)
+Router.post('/', create_employee)
+Router.put('/:id', update_employee)
+Router.delete('/:id', delete_employee)
+
+
+module.exports = Router
