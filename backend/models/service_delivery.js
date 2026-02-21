3dbcddb5beef59b61ec6a9c3b2994a692b18f10e
uparfait <parfaituwayo@gmail.com>
2026-02-21 12:57:25 +0200

    Adding meaningfully attributes in service delivery model

--- backend/models/service_delivery.js
+++ backend/models/service_delivery.js
@@ -5,25 +5,85 @@ const service_delivery_schema = new mongoose.Schema({
         id_type: String,
         number: String
     },
+    vehicle_storage: {
+        has_vehicle: { type: Boolean, default: false },
+        vehicle_details: {
+            plate_number: String,
+            entered_time: Date,
+            exited_time: Date,
+            duration: String,
+        }
+    },
     full_name: { type: String },
     telephone: { type: String },
     email: { type: String },
-    department_name: { type: String },
-    department_id: { type: String },
-    date: { type: Date, default: Date.now },
+    departments_assigned: [
+        {
+            department_id: String,
+            department_name: String,
+            assigned_time: { type: Date, default: Date.now },
+            reached_in: { type: Boolean, default: false },
+            provider_name: String
+        }
+    ],
+    entry_date: { type: Date, default: Date.now },
     gender: { type: String },
     durations: {
-        service_duration: String,
+        services_durations: [
+            {
+                department_id: String,
+                department_name: String,
+                duration: String,
+                started_at: Date,
+                ended_at: Date,
+                provider_name: String
+            }
+        ],
         entry_duration: String,
-        emergency_duration: String
+        emergency_durations: [
+            {
+                type_of_emergency: {
+                    type: String,
+                    enum: ['Transifer to another department', 'Leave outside', 'Other'],
+                    default: 'Other'
+                },
+
+                duration: String,
+                started_at: Date,
+                ended_at: Date,
+                provider_name: String
+            }
+        ]
     },
-    items: { type: String },
-    status: { 
-        type: String, 
-        enum: ['pending', 'inprogress', 'transfered', 'completed'],
-        default: 'pending'
-    },
-    notes: [String],
+    items_entered_with: [
+        {
+            item_name: String,
+            quantity: Number,
+            description: { type: String, default: "Not specified" }
+        }
+    ],
+    items_exited_with: [
+        {
+            item_name: String,
+            quantity: Number,
+            description: { type: String, default: "Not specified" }
+        }
+    ],
+    services_status: [
+        {
+            department_name: String,
+            department_id: String,
+            provider_name: String,
+            type: String,
+            enum: ['not started', 'inprogress', 'transfered', 'completed'],
+            default: 'not started'
+        }
+    ],
+    notes: [{
+        writter_name: String,
+        message: String,
+        timestamp: { type: Date, default: Date.now }
+    }],
     registered_by: { type: String }
 });
 
