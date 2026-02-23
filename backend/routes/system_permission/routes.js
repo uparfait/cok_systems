ff04790ffffd1e6bf353bc7cedc34b41d2971928
uparfait <parfaituwayo@gmail.com>
2026-02-23 10:29:07 +0200

    Replacing instance to origina class as static method used

--- backend/routes/system_permission/routes.js
+++ backend/routes/system_permission/routes.js
@@ -5,15 +5,15 @@
 const Router = require('express').Router()
 const SystemPermissionManager = require('../../controllers/system_permission/manage.js')
 
-const ManagerInstance = new SystemPermissionManager()
 
 
 
-Router.get('/', ManagerInstance.list) // List all system permissions
-Router.get('/:id', ManagerInstance.getById) // Get system permission by ID
-Router.post('/', ManagerInstance.create) // Create a new system permission
-Router.put('/:id', ManagerInstance.update) // Update a system permission by ID
-Router.delete('/:id', ManagerInstance.delete) // Delete a system permission by ID
+
+Router.get('/', SystemPermissionManager.list) // List all system permissions
+Router.get('/:id', SystemPermissionManager.getById) // Get system permission by ID
+Router.post('/', SystemPermissionManager.create) // Create a new system permission
+Router.put('/:id', SystemPermissionManager.update) // Update a system permission by ID
+Router.delete('/:id', SystemPermissionManager.delete) // Delete a system permission by ID
 
 
 module.exports = Router
