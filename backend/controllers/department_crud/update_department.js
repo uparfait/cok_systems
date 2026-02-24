6f6b7f3d7f4a0688c5910943c9dc8a314e44a685
uparfait <parfaituwayo@gmail.com>
2026-02-24 08:18:15 +0200

    Solving some issues in department crud

--- backend/controllers/department_crud/update_department.js
+++ backend/controllers/department_crud/update_department.js
@@ -5,11 +5,12 @@ const user_model = require('../../models/user.js')
 module.exports = async function update_department(req, res, next) {
     try {
         const { id } = req.params
+        // oly allow to change reader and response time only
         let {
-            department_id = null,
-            department_name = null,
+            department_response_time_in_minutes = 0,
             department_leader = null // Expecting an email string
         } = req.body || {}
+            let leader_user = null
 
         // Validate if the ID is a valid MongoDB ObjectId
         if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -30,21 +31,10 @@ module.exports = async function update_department(req, res, next) {
             })
         }
 
-        if (
-            department_id === null &&
-            department_name === null &&
-            department_leader === null
-        ) {
-            return res.status(400).json({
-                success: false,
-                type: "warning",
-                message: "No valid data provided to update",
-            })
-        }
 
         // Verify and load user data if a new leader email was provided
         if (department_leader !== null && department_leader !== undefined) {
-            const leader_user = await user_model.findOne({ email: department_leader })
+             leader_user = await user_model.findOne({ email: department_leader })
             
             if (!leader_user) {
                 return res.status(404).json({
@@ -61,25 +51,10 @@ module.exports = async function update_department(req, res, next) {
                 picture: leader_user.picture || ""
             }
         }
-
-        // Validate and update other allowed fields
-        if (department_name !== null && department_name !== undefined) department.department_name = department_name
-        if (department_id !== null && department_id !== undefined) department.department_id = department_id.toString().toUpperCase()
-
+        department.department_response_time_in_minutes = department_response_time_in_minutes
         department.registered_by = req.user?.name || "Not specified"
 
         const saved_department = await department.save()
-
-        // Also update all employees whose department matches this one
-        await user_model.updateMany(
-            { department_id: department.department_id }, // assuming you tie users by string ID based on your user schema
-            {
-                $set: {
-                    department_name: saved_department.department_name,
-                    department_id: saved_department.department_id
-                }
-            }
-        )
 
         return res.status(200).json({
             success: true,
