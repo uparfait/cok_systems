ab9c14eb632cd86ca36e9071ff828115baeea182
uparfait <parfaituwayo@gmail.com>
2026-02-24 08:25:59 +0200

    Adding department_response_time_in_minutesto a model

--- backend/models/department.js
+++ backend/models/department.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const department_schema = new mongoose.Schema({
     department_name: { type: String, required: true },
+    department_response_time_in_minutes: { type: Number, default: 0 },
     department_id: { type: String, unique: true },
     created_date: { type: Date, default: Date.now },
     // Updated department_leader to hold an object
