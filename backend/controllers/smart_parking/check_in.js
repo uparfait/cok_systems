bb08d25be8d39d4e72c949377c2fa97d75415a08
uparfait <parfaituwayo@gmail.com>
2026-03-11 15:50:15 +0200

    Restricting un-allowed driver type

--- backend/controllers/smart_parking/check_in.js
+++ backend/controllers/smart_parking/check_in.js
@@ -26,6 +26,16 @@ module.exports = async function car_check_in(req, res, next) {
             })
         }
 
+        const allowed_driver_type = ['regular', 'vistor', 'staff']
+
+        if(!allowed_driver_type.includes(driver_type.toLowerCase())) {
+            return res.status(400).json({
+                success: false,
+                type: 'warning',
+                message: "Invalid driver type allowed types are Regular, Visitor, Staff"
+            })
+        }
+
         // Normalize plate number
         plate_number = plate_number.toString().toUpperCase().replace(/\s+/g, '')
 
