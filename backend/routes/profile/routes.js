e2efd153ad7a64015b29670ec251e10184fad05a
uparfait <parfaituwayo@gmail.com>
2026-04-08 11:47:02 +0200

    Adding profile route

--- backend/routes/profile/routes.js
+++ backend/routes/profile/routes.js
@@ -1,15 +1,7 @@
-/**
- * Profile routes - User profile management
- */
-
 const Router = require('express').Router();
 const changePasswordController = require('../../controllers/profile/change_password');
 const authenticate = require('../../middlewares/authenticate');
 
-/**
- * POST /profile/change-password
- * Change user password with validation
- */
 Router.post('/change-password', authenticate, changePasswordController);
 
 module.exports = Router;
