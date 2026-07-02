c4038bcbb55b1c2f073b82c69dac28089c14394b
uparfait <parfaituwayo@gmail.com>
2026-07-02 15:31:16 +0200

    Adding cashes

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'cok-systems-v3';
-const STATIC_CACHE = 'cok-systems-static-v3';
-const DYNAMIC_CACHE = 'cok-systems-dynamic-v3';
+const CACHE_NAME = 'cok-systems-v4';
+const STATIC_CACHE = 'cok-systems-static-v4';
+const DYNAMIC_CACHE = 'cok-systems-dynamic-v4';
 
 // Resources to cache immediately
 const STATIC_ASSETS = [
