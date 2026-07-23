1b0754aae43f4cb6316ac9388e8ae972508a6455
uparfait <parfaituwayo@gmail.com>
2026-07-23 12:39:12 +0200

    Adding host url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 
   server: {
     port: 5173,
-    host: process.env.VITE_DEV_SERVER_HOST || false,
+    host: process.env.VITE_DEV_SERVER_HOST || '0.0.0.0',
     proxy: {
       '/cok/api/v1': {
         
