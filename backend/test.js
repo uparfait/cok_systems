5e82180949118667b8d12e933f95209ce78bab77
uparfait <parfaituwayo@gmail.com>
2026-07-24 16:29:37 +0200

    Deployement giudance

--- backend/test.js
+++ backend/test.js
@@ -1,10 +1,40 @@
-const sendLaunchInviteEmail = require('./utilities/email').sendLunchInviteEmail;
+const nodemailer = require('nodemailer');
 
-sendLaunchInviteEmail('parfaituwayo@gmail.com', "Uwayo parfait").then((result) => {
-    if (result.success) {
-        console.log(result);
-    }
-}).catch((error) => {
-    console.error('Error sending lunch invite email:', error);
-}
-)
+const createTransporter = () => {
+    const transporter = nodemailer.createTransport({
+        host: "mail.kigalicity.gov.rw",
+        port: 25,
+        secure: false, 
+        auth: {
+            user: "coksystems@kigalicity.gov.rw",
+            pass: "CTown@2025!&"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    
+    transporter.verify((error, success) => {
+        if (error) {
+            console.error('SMTP Connection Error:', error);
+        } else {
+            console.log('SMTP Server is ready');
+        }
+    });
+    
+    return transporter;
+};
+
+
+const receiver = "parfaituwayo@gmail.com"
+const message = "Test SMTP email"
+const Subject = "TEST"
+
+// send a message
+createTransporter().sendMail({
+    from: '"IKAZE" <coksystems@kigalicity.gov.rw>',
+    subject: Subject,
+    to: receiver,
+    text: message
+    
+})
